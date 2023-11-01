0fce9eb03e2b011f91dd20fc532db91ed1679dff
thatfoogabriel <sotogabriel1228@gmail.com>
2023-11-01 13:43:41 -0700

    finished up to chapter 12

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
@@ -0,0 +1,18 @@
+package com.bignerdranch.android.criminalintent
+
+import android.content.Context
+
+class CrimeRepository private constructor(context: Context) {
+    companion object {
+        private var INSTANCE: CrimeRepository? = null
+        fun initialize(context: Context) {
+            if (INSTANCE == null) {
+                INSTANCE = CrimeRepository(context)
+            }
+        }
+        fun get(): CrimeRepository {
+            return INSTANCE ?:
+            throw IllegalStateException("CrimeRepository must be initialized")
+        }
+    }
+}
